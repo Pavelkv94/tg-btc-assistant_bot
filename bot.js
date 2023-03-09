4d25275f26119779360984d83962a7fc3019ac00
Pavel Kazlou <pavel.kv94@yandex.by>
2023-03-09 14:13:22 +0300

    hide env var's

--- bot.js
+++ bot.js
@@ -2,7 +2,8 @@ const TelegramApi = require("node-telegram-bot-api");
 
 
 //наш токен
-const token = '6176432596:AAFIcsS-QcrHoZwFcrvnyZzSg36Z-a6vq6o'
+// const token = '6176432596:AAFIcsS-QcrHoZwFcrvnyZzSg36Z-a6vq6o';
+const token = process.env.BOT_TOKEN;
 //создали бота
 const bot = new TelegramApi(token, { polling: true })
 
