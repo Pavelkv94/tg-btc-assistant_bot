13917be078c481a7b211637f03798d9bf74636eb
Pavel Kazlou <pnkazlou@ibagroup.eu>
2025-12-14 19:52:56 +0100

    fix

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,6 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
 
-const token = "8045482326:AAFApZ5gqb_Ri-F-x22pMtJ5QoWcGHUeuYk" //process.env.TG_BOT_TOKEN;
+const token = process.env.TG_BOT_TOKEN;
 
 if (!token) {
   throw new Error("❌ Telegram Bot Token not provided. Please set TG_BOT_TOKEN in environment variables.");
